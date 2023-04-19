8094ab944da27ef10729e1aa84719ef599ef487b
Juozas leilionas <busuko@yahoo.com>
2023-04-19 21:19:45 +0300

    +

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -31,10 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'beauty.apps.BeautyConfig',
-    # 'tinymce',
-    'crispy_forms',
+    'beauty',
     'crispy_bootstrap4',
+     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
